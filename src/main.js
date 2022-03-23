81f3c5488a092abc5ba38b779a8a8573542887dc
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-22 22:49:47 -0600

    css update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 // CSS
 import "./assets/tailwind.css";
+
 // Read the docs: https://fontawesome.com/v6/docs/web/use-with/vue/add-icons
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBarsStaggard as faBars } from "@fortawesome/pro-solid-svg-icons";
