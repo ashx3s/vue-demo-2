40faf3c2b6d3da4cbbd828af6e1157a93670a95b
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-22 22:55:28 -0600

    cooler x icon

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 
 // Specific Icons
 import { faBarsStaggard as faBarsStaggard } from "@fortawesome/pro-solid-svg-icons";
-import { faXmark as faXmark } from "@fortawesome/pro-solid-svg-icons";
+import { faCircleXmark as faCircleXmark } from "@fortawesome/pro-solid-svg-icons";
 import { faToggleOn as faToggleOn } from "@fortawesome/pro-solid-svg-icons";
 import { faToggleOff as faToggleOff } from "@fortawesome/pro-solid-svg-icons";
 
-library.add(faBarsStaggard, faXmark, faToggleOn, faToggleOff);
+library.add(faBarsStaggard, faCircleXmark, faToggleOn, faToggleOff);
 
 createApp(App).use(router).mount("#app");
