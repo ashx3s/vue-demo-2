fc68884cc0cb1e794c7874a6d69b2dbcc5f536a8
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-22 23:03:47 -0600

    updated toggle

--- src/components/ui/TheToggle.vue
+++ src/components/ui/TheToggle.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+  <font-awesome-icon
+    icon="toggle-off"
+    class="bg-slate-100 text-slate-900 hover:bg-emerald-100 hover:shadow-sm hover:shadow-emerald-100 m-1"
+  />
+</template>
+<script setup></script>
