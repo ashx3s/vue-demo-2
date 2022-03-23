5e38ba152e177922050538ade1b6a78eb4c74ef8
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-22 22:30:53 -0600

    card action style

---
+++ src/components/cards/BaseCardAction.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="props.url"
+    class="grid place-items-center px-4 py-2 bg-blue-900 hover:bg-blue-600 hover:shadow-sm hover:shadow-blue-200 hover:ease-in-out active:bg-emerald-700"
+    ><slot></slot
+  ></a>
+</template>
+
+<script setup>
+const props = defineProps({
+  url: {
+    type: String,
+  },
+});
+</script>
